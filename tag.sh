7984d497b2fecaf18fdc43c62d0277ed27637f30
Sudersan Raghul <sudersan.raghulmakesh@agoda.com>
2019-07-09 14:26:13 +0700

    fix tag

--- tag.sh
+++ tag.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker tag simple-go-server:$(date +%Y%m%d%H%M) simple-go-server:$GO_PIPELINE_LABEL -f Dockerfile .
+docker tag simple-go-server:$(date +%Y%m%d%H%M) simple-go-server:$GO_PIPELINE_LABEL
